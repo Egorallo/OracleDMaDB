733d805edc769d65b7533a38e6a2cb7a36f40d40
Egorallo <sea25042003@gmail.com>
2024-02-20 22:40:01 +0300

    Lab1/task2 completed

---
+++ Lab1/task2.sql
@@ -0,0 +1,13 @@
+DECLARE
+    var_id NUMBER;
+    var_val NUMBER;
+BEGIN
+    FOR i IN 1..10000 LOOP
+        var_id := i;
+        var_val := ROUND(DBMS_RANDOM.VALUE(1, 10000));
+
+        INSERT INTO MyTable (id, val) VALUES (var_id, var_val);
+    END LOOP;
+END;
+
+
