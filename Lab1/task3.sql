0fc10cf61e8d7d8aa8c5f43b39384a150d218a41
Egorallo <sea25042003@gmail.com>
2024-02-20 23:01:01 +0300

    Lab1/task3 completed

---
+++ Lab1/task3.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION check_even_odd
+RETURN VARCHAR2
+IS
+    var_even_count NUMBER := 0;
+    var_odd_count NUMBER := 0;
+BEGIN
+    SELECT COUNT(CASE WHEN MOD(val, 2) = 0 THEN 1 END),
+           COUNT(CASE WHEN MOD(val, 2) <> 0 THEN 1 END)
+    INTO var_even_count, var_odd_count
+    FROM MyTable;
+
+    IF var_even_count > var_odd_count THEN
+        RETURN 'TRUE';
+    ELSIF var_even_count < var_odd_count THEN
+        RETURN 'FALSE';
+    ELSE
+        RETURN 'EQUAL';
+    END IF;
+END;
+/
+
+DECLARE
+    v_result VARCHAR2(10);
+BEGIN
+    v_result := check_even_odd();
+
+    DBMS_OUTPUT.PUT_LINE('Результат: ' || v_result);
+END;
+/
+
