afe18f3cf88fd5258596897f3bed309548a5e691
Egorallo <sea25042003@gmail.com>
2024-02-20 23:15:46 +0300

    Lab1/task4 completed

---
+++ Lab1/task4.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION generate_insert_command(
+    p_id IN NUMBER,
+    p_val IN NUMBER
+) RETURN VARCHAR2
+IS
+    v_insert_command VARCHAR2(1000);
+BEGIN
+    v_insert_command := 'INSERT INTO MyTable (id, val) VALUES (' || p_id || ', ' || p_val || ');';
+
+    DBMS_OUTPUT.PUT_LINE(v_insert_command);
+
+    RETURN v_insert_command;
+END;
+/
+
+DECLARE
+    v_insert_cmd VARCHAR2(1000);
+BEGIN
+    v_insert_cmd := generate_insert_command(120, 222);
+END;
+/
